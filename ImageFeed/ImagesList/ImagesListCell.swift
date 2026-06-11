b73797dc604c0269cfb3dec0b3d5033e658d1848
alizodasabrina <alizodasabrina@gmail.com>
2026-06-11 22:08:29 +0500

    Add profile screen

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,42 +8,63 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+
+    // MARK: - Constants
+
     static let reuseIdentifier = "ImagesListCell"
 
-    @IBOutlet private var cellImage: UIImageView!
-    @IBOutlet private var likeButton: UIButton!
-    @IBOutlet private var dateLabel: UILabel!
+    private enum Constants {
+        static let gradientHeight: CGFloat = 30
+        static let gradientLocation: NSNumber = 0.5393
+    }
+
+    // MARK: - IBOutlets
+
+    @IBOutlet private weak var photoImageView: UIImageView!
+    @IBOutlet private weak var likeButton: UIButton!
+    @IBOutlet private weak var dateLabel: UILabel!
+
+    // MARK: - Properties
 
     private let gradientLayer = CAGradientLayer()
+
+    // MARK: - Lifecycle
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        gradientLayer.colors = [
-            UIColor(red: 26 / 255, green: 27 / 255, blue: 34 / 255, alpha: 0).cgColor,
-            UIColor(red: 26 / 255, green: 27 / 255, blue: 34 / 255, alpha: 0.2).cgColor
-        ]
-        gradientLayer.locations = [0, 0.5393]
-        cellImage.layer.addSublayer(gradientLayer)
+        setupGradient()
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        let gradientHeight: CGFloat = 30
         gradientLayer.frame = CGRect(
             x: 0,
-            y: cellImage.bounds.height - gradientHeight,
-            width: cellImage.bounds.width,
-            height: gradientHeight
+            y: photoImageView.bounds.height - Constants.gradientHeight,
+            width: photoImageView.bounds.width,
+            height: Constants.gradientHeight
         )
     }
 
+    // MARK: - Public Methods
+
     func configure(image: UIImage, date: String, isLiked: Bool) {
-        cellImage.image = image
+        photoImageView.image = image
         dateLabel.text = date
 
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         likeButton.setImage(likeImage, for: .normal)
+    }
+
+    // MARK: - Private Methods
+
+    private func setupGradient() {
+        gradientLayer.colors = [
+            UIColor(red: 26 / 255, green: 27 / 255, blue: 34 / 255, alpha: 0).cgColor,
+            UIColor(red: 26 / 255, green: 27 / 255, blue: 34 / 255, alpha: 0.2).cgColor
+        ]
+        gradientLayer.locations = [0, Constants.gradientLocation]
+        photoImageView.layer.addSublayer(gradientLayer)
     }
 }
---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Sabrina Mavlyanova on 11/06/26.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func didTapLogoutButton() {
+    }
+}
